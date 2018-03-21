0321783f6a0b0bb2668189a089db77685e87d2b1
Fathir Qisthi <3runrunrun@gmail.com>
2018-03-21 20:28:48 +0700

    D8 versi fathir 8

--- application/controllers/transaction/C_Fixed_Cost_Ratio.php
+++ application/controllers/transaction/C_Fixed_Cost_Ratio.php
@@ -29,8 +29,7 @@ class C_Fixed_Cost_Ratio extends CI_Controller {
   public function show($id)
   {
     $data['detail'] = $this->Detailer->show($id);
-    $data['produk'] = $this->Achievement->show($id);
-
+    $data['produk'] = $this->Achievement->show_produk($id, 'a.id_outlet, c.nama_outlet, a.id_produk, d.nama as nama_produk, a.nominal_target, a.nominal_jumlah, nominal_jumlah_januari, nominal_jumlah_februari, nominal_jumlah_maret, nominal_jumlah_april, nominal_jumlah_mei, nominal_jumlah_juni, nominal_jumlah_juli, nominal_jumlah_agustus, nominal_jumlah_september, nominal_jumlah_oktober, nominal_jumlah_november, nominal_jumlah_desember');
     $this->load->view('heads/head-form-table-print');
     $this->load->view('navbar');
     $this->load->view('contents/transaction/fixed-cost/detail', $data);
